2baad1650fc86014e453e15b394599ded5a3a1fe
victorjaquez <victor@jaquez.me>
2019-03-07 20:46:37 -0700

    created doughbut component, initial data, labels, html canvas, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { ChartsModule } from 'ng2-charts';
+import { RouterModule } from '@angular/router';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoughnutChartComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
