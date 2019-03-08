7aef21633d055727c9b0ac5eda87b4d41cc65916
victorjaquez <victor@jaquez.me>
2019-03-07 20:06:40 -0700

    added data and chart array properties, and url property for mock json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-
+import { Chart } from 'chart.js';
+import { Data } from './Data'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-charts';
+  data: Data[]; // 
+  url = 'http://localhost:4000/results';
+  month = [];
+  price = [];
+  chart = [];
   constructor(private httpClient: HttpClient) {}
+
+
 }
