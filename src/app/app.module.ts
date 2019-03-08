8bdf258cb1178a49a65d98c7f8cb07c9f8a86af4
victorjaquez <victor@jaquez.me>
2019-03-07 20:52:15 -0700

    routing, needs fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component'
+
+const routes: Routes = [
+  {path: 'doughnut-chart', component: DoughnutChartComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
